7f784641e4453a494f7be01d728e4b67a6e2c3cb
xiaode <liyangde66@gmail.com>
2016-11-10 21:12:47 +0800

    init

---
+++ app/src/main/java/com/example/xiaode/emoi/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.xiaode.emoi;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
